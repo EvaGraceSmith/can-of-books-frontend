e5293a4ff71cf9a1e6e0b11eea872864f375e3b3
EvaGraceSmith <smithevagrace@gmail.com>
2023-02-14 17:55:27 -0600

    added cards to about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import axios from 'axios';
 // import Carousel from 'react-bootstrap/Carousel';
 import './About.css';
+import Header from './Header.js';
+import Card from 'react-bootstrap/Card';
 
 class Profile extends React.Component {
     constructor(props) {
@@ -15,13 +17,35 @@ class Profile extends React.Component {
     render() {
         return (
             <>
-                  <a href = '/'>Home</a>
+                <a href='/'>Home</a>
                 <main>
+                    <Header />
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Chris Vander Linden</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Full Stack Software Developer</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Linked In</Card.Link>
+                            <Card.Link href="#">Github</Card.Link>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Eva Grace Smith</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Full Stack Software Developer</Card.Subtitle>
+                            <Card.Text>
+                            Using my love of web design, art, and people to make the world a better place, one page at a time.
+                            </Card.Text>
+                            <Card.Link href="https://www.linkedin.com/in/eva-grace-smith-99661125b/">Linked In</Card.Link>
+                            <Card.Link href="https://github.com/EvaGraceSmith">Github</Card.Link>
+                        </Card.Body>
+                    </Card>
                     <h1>About Us</h1>
-                    <h2>Chris Vander Linden</h2>
-                    <h3>Software Developer</h3>
-                    <h2>Eva Grace Smith</h2>
-                    <h3>Software Developer</h3>
+
 
                 </main>
             </>
