62e311ab4c60232c2876b0457a66a59ce4d529b2
EvaGraceSmith <smithevagrace@gmail.com>
2023-02-17 19:30:07 -0600

    updated index.js and header.js removing redundant and commented out code

--- src/index.js
+++ src/index.js
@@ -16,39 +16,18 @@ import Home from './components/Home.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <App /> } errorElement={ <Error /> }>
-      <Route path="/" element={ <Home /> } />
-      <Route path="/books" element={ <BestBooks /> } />
-      <Route path="/about" element={ <About /> } />
+    <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<BestBooks />} />
+      <Route path="/about" element={<About />} />
     </Route>
   )
 );
 
-
-
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App/>,
-//   },
-
-//   {
-//     path: "/about",
-//     element: <About/>,
-//   },
-// ]);
-
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
