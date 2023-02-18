9270f064d1b027ae4bdaccd2d5caadfdb184dc64
EvaGraceSmith <smithevagrace@gmail.com>
2023-02-17 19:27:36 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
 import React from 'react';
-// import axios from "axios";
 import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-// import BestBooks from './components/BestBooks.js';
-// import { Routes, Route } from 'react-router-dom';
-// import About from './components/About.js';
 import { Outlet } from "react-router-dom";
 
 
@@ -14,33 +10,16 @@ import { Outlet } from "react-router-dom";
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    };
-  }
+
   render() {
     return (
       <>
 
-<Header />
+        <Header />
         <Outlet />
-
         <Footer />
 
-        {/* <Router> */}
-          {/* <Header /> */}
-          {/* <Routes>
-            <Route path='/' element={<Header />}>
-              <Route index element={<App />} />
-              <Route path="about" element={<About />} />
-            </Route>
-          </Routes>
-
-          <BestBooks /> */}
-
-        {/* </Router> */}
       </>
     );
   }
