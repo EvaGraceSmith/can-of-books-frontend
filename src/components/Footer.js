e1fb3e2a0a6b697b623eee516345441d6392109b
EvaGraceSmith <smithevagrace@gmail.com>
2023-02-17 19:20:16 -0600

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import Navbar from 'react-bootstrap/Navbar';
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand> &copy; 2023 Eva Grace Smith & Chris Vander Linden</Navbar.Brand>
       </Navbar>
     );
   }
