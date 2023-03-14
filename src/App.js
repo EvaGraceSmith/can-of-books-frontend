f1ab7a60aa91d295c09bcc9ab8024305325232f6
EvaGraceSmith <smithevagrace@gmail.com>
2023-03-13 23:02:56 -0500

    removed auth0

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import { Outlet } from "react-router-dom";
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
-let SERVER = process.env.REACT_APP_SERVER;
+// let SERVER = process.env.REACT_APP_SERVER;
 
 
-let AUTHSERVER = process.env.REACT_APP_DOMAIN;
-let yourClientId = process.env.REACT_APP_CLIENTID;
+// let AUTHSERVER = process.env.REACT_APP_DOMAIN;
+// let yourClientId = process.env.REACT_APP_CLIENTID;
 
 class App extends React.Component {
 
 
   render() {
     return (
-      <Auth0Provider
-        domain={AUTHSERVER}
-        clientId={yourClientId}
-        SERVER={SERVER}
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}>
+      // <Auth0Provider
+      //   domain={AUTHSERVER}
+      //   clientId={yourClientId}
+      //   SERVER={SERVER}
+      //   authorizationParams={{
+      //     redirect_uri: window.location.origin
+      //   }}>
       <>
 
         <Header />
@@ -31,7 +31,7 @@ class App extends React.Component {
         <Footer />
 
       </>
-      </Auth0Provider>
+      // </Auth0Provider>
     );
   }
 }
